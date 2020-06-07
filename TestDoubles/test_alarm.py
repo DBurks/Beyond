f8d5d87cd809d17bff61f62cac87211a236911b1
damien burks <damien.burks.1@gmail.com>
2020-06-07 04:59:25 -0600

    Monkeypatching

--- TestDoubles/test_alarm.py
+++ TestDoubles/test_alarm.py
@@ -1,6 +1,6 @@
 from alarm import Alarm
 from sensor import Sensor
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 import pytest
 
 @pytest.fixture()
@@ -41,3 +41,23 @@ def test_normal_pressure_alarm_stays_off(stub_normal_sensor):
     alarm = Alarm(stub_normal_sensor)
     alarm.check()
     assert not alarm.is_alarm_on
+
+def test_alarm_with_high_pressure_value():
+    with patch('alarm.Sensor') as test_sensor_class:
+        test_sensor_instance = Mock()
+        test_sensor_instance.sample_pressure.return_value = 22
+        test_sensor_class.return_value = test_sensor_instance
+    
+        alarm = Alarm()
+        alarm.check()
+        assert alarm.is_alarm_on
+
+@patch('alarm.Sensor')
+def test_alarm_with_too_low_pressure_value(test_sensor_class):
+    test_sensor_instance = Mock()
+    test_sensor_instance.sample_pressure.return_value = 16
+    test_sensor_class.return_value = test_sensor_instance
+
+    alarm = Alarm()
+    alarm.check()
+    assert alarm.is_alarm_on
